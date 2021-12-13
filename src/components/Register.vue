3468f64d98374c8a9f2e74bcd796ca7a464b2b96
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-12-13 09:55:40 +0100

    inizio register

---
+++ src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="row justify-content-center">
+        <form action="" method="post" class="form-container col-8">
+            <div class="row form-section"> 
+                <label for="username" class="col-12">Username</label>
+                <input type="text" id="username" name="username" class="col-12">
+            </div>
+
+            <div class="row form-section">
+                <label for="email" class="col-12">Email</label>
+                <input type="email" id="email" name="email" class="col-12">
+            </div>
+
+            <div class="row form-section">
+                <label for="password" class="col-12">Password</label>
+                <input type="password" name="password" id="password" class="col-12">
+            </div>
+
+            <div class="form-section">
+                <input type="submit" value="Registrati" class="btn btn-primary"> 
+            </div>
+        </form>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+
+
+
+</style>>
